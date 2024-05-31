9736607812fb14aeed91cdd0f719a6b442bb3458
GENCAY KAZIM SELCUK <121198224+gencayks@users.noreply.github.com>
2024-05-31 19:43:48 +0200

    updated sloth machine

--- slotmachine.py
+++ slotmachine.py
@@ -8,3 +8,25 @@ print(results)
 
 if all(symbol == 'cherry' for symbol in results):
     print("Cherry Jackpot!")
+if all(symbol == 'grapes' for symbol in results):
+    print("Grapes Jackpot!")
+if all(symbol == 'watermelon' for symbol in results):
+    print("Watermelon Jackpot!")
+if all(symbol == 'seven' for symbol in results):
+    print("Seven Jackpot!")
+    
+else:
+    print("Try again!")
+    
+
+def play():
+    while True:
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != 'yes':
+            break
+        results = random.choices(symbols , k = 3)
+        print(results)
+        if all(symbol == 'cherry' for symbol in results):
+            print("Cherry Jackpot!")
+        else:
+            print("Try again!")
